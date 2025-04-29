c7bfc6aa704890d98707c07be784c1d7313ab3a9
Patron <Patron@Patrons-MacBook-Air-3.local>
2025-04-29 02:28:56 -0500

    stupid euler

--- HW07/matmul.cu
+++ HW07/matmul.cu
@@ -2,31 +2,33 @@
 #include <iostream>
 #include "matmul.cuh"
 
-template <typename T, unsigned int block_dim>
+template <typename T>
 __global__ void matmul_kernel(
     const T *A, T *B, T *C, 
-    unsigned int n
+    unsigned int n, unsigned int block_dim
 ) {
-    __shared__ T tile_A[block_dim][block_dim];
-    __shared__ T tile_B[block_dim][block_dim];
+    extern __shared__ T shared[];
+    T *tile_A = shared;
+    T *tile_B = shared + block_dim * block_dim;
+
     uint round = (n + block_dim - 1) / block_dim;
     uint row = blockIdx.y * block_dim + threadIdx.y;
     uint col = blockIdx.x * block_dim + threadIdx.x;
     C[row * n + col] = 0;
     for(int i = 0; i < round; i++){
         if (i*block_dim + threadIdx.x < n && row < n) {
-            tile_A[threadIdx.y][threadIdx.x] = A[row * n + i * block_dim + threadIdx.x];
+            tile_A[threadIdx.y * block_dim + threadIdx.x] = A[row * n + i * block_dim + threadIdx.x];
         } else {
-            tile_A[threadIdx.y][threadIdx.x] = 0;
+            tile_A[threadIdx.y * block_dim + threadIdx.x] = 0;
         }
         if (i*block_dim + threadIdx.y < n && col < n) {
-            tile_B[threadIdx.y][threadIdx.x] = B[(i * block_dim + threadIdx.y) * n + col];
+            tile_B[threadIdx.y * block_dim + threadIdx.x] = B[(i * block_dim + threadIdx.y) * n + col];
         } else {
-            tile_B[threadIdx.y][threadIdx.x] = 0;
+            tile_B[threadIdx.y * block_dim + threadIdx.x] = 0;
         }
         __syncthreads();
         for (int j = 0; j<block_dim; j++){
-            C[row * n + col] += tile_A[threadIdx.y][j] * tile_B[j][threadIdx.x];
+            C[row * n + col] += tile_A[threadIdx.y * block_dim + j] * tile_B[j * block_dim + threadIdx.x];
         }
         __syncthreads();
     }
@@ -45,7 +47,7 @@ __host__ void matmul_1(
     cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
     dim3 block(block_dim, block_dim);
     dim3 grid((n + block_dim - 1) / block_dim, (n + block_dim - 1) / block_dim);
-    matmul_kernel<int, block_dim> <<<grid, block>>>(d_A, d_B, d_C, n);
+    matmul_kernel<int> <<<grid, block>>>(d_A, d_B, d_C, n, block_dim);
     cudaDeviceSynchronize();
     cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
     cudaFree(d_A);
@@ -66,7 +68,7 @@ __host__ void matmul_2(
     cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
     dim3 block(block_dim, block_dim);
     dim3 grid((n + block_dim - 1) / block_dim, (n + block_dim - 1) / block_dim);
-    matmul_kernel<float, block_dim> <<<grid, block>>>(d_A, d_B, d_C, n);
+    matmul_kernel<float> <<<grid, block>>>(d_A, d_B, d_C, n, block_dim);
     cudaDeviceSynchronize();
     cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
     cudaFree(d_A);
@@ -87,7 +89,7 @@ __host__ void matmul_3(
     cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
     dim3 block(block_dim, block_dim);
     dim3 grid((n + block_dim - 1) / block_dim, (n + block_dim - 1) / block_dim);
-    matmul_kernel<double, block_dim> <<<grid, block>>>(d_A, d_B, d_C, n);
+    matmul_kernel<double> <<<grid, block>>>(d_A, d_B, d_C, n, block_dim);
     cudaDeviceSynchronize();
     cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
     cudaFree(d_A);
